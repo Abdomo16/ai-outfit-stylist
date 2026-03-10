acbdf0245be8948a52461145b69363989649efaf
Abdomo16 <abdoandali12@gmail.com>
2026-03-10 09:53:20 +0200

    add global bloc providers for auth and wardrobe

--- lib/app/app_providers.dart
+++ lib/app/app_providers.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../data/repositories/auth_repository.dart';
 import '../features/auth/cubit/auth_cubit.dart';
+import '../data/repositories/wardrobe_repository_impl.dart';
+import '../features/wardrobe/cubit/wardrobe_cubit.dart';
 
 class AppProviders extends StatelessWidget {
   final Widget child;
@@ -14,6 +16,9 @@ class AppProviders extends StatelessWidget {
       providers: [
         BlocProvider<AuthCubit>(
           create: (context) => AuthCubit(AuthRepository())..checkAuthStatus(),
+        ),
+        BlocProvider<WardrobeCubit>(
+          create: (context) => WardrobeCubit(WardrobeRepositoryImpl()),
         ),
       ],
       child: child,
